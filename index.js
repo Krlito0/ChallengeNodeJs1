8eed8900c30ad9b373029a576339621b6c7220cf
Krlito0 <cgnakine@gmail.com>
2023-04-17 14:19:34 +0200

    mise a jour dialogue vache

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const info = require("./information.js");
 var cowsay = require("cowsay");
 
 console.log(cowsay.say({
-    text : (`Bonjour je m'appelle ${info.student} j'ai actuellement ${info.age} ans et j'étudie dans la célèbre école de ${info.campus}`),
-    e : "oO",
+    text : (`Meuuuhhhjour je m'appelle ${info.student} j'ai actuellement ${info.age} meuhans et j'étudie dans la célèbre meuuhhcole de ${info.campus}`),
+    e : "^ ^",
     T : "U "
 }));
